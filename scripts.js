2ef63d6bca6f26594f021638a3dd323cc233939b
AnetaH <aneta.halz@interia.pl>
2017-02-28 11:39:11 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
-// plik scripts.js
-
-
 var a = 3,
-	b = 10;
+    b = 10;
 
 var value = (a * a) + (2 * a * b) - (b * b);
 console.log('wynik ' + value);
 
 if (value === 0) { 
-	console.log('wynik jest równy 0');
+  console.log('wynik jest równy 0');
 } else if (value > 0) {
-	console.log('wartość dodatnia');
+  console.log('wartość dodatnia');
 } else {
-	console.log('wartość ujemna');
+  console.log('wartość ujemna');
 }
